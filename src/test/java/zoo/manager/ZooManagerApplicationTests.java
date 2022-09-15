df706f2111fd751e3c3b5404ffdbeb38909a81d3
Garin1998 <kkubis44386@gmail.com>
2022-09-15 12:16:16 +0200

    added Spring Initializr

---
+++ src/test/java/zoo/manager/ZooManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package zoo.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
